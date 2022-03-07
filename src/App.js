86fc884e7379d78f2c9137bf6cd0c710c91c5043
j_esteve <javier.esteve@ultratendency.com>
2022-03-07 15:25:25 +0100

    using react router to have different urls in the app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 import { Suspense } from 'react';
 import LeftMenu from './LeftMenu';
 import TopMenu from './TopMenu';
+import WorkInProgress from './WorkInProgress';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <LeftMenu />
-      <TopMenu />
-      <div id="content" className="content"/>
+      <BrowserRouter>
+        <LeftMenu />
+        <TopMenu />
+        <div id="content" className="content">
+          <Routes>
+            <Route path="/" element={<WorkInProgress component="Home"/>} />
+            <Route path="/services" element={<WorkInProgress component="Services"/>} />
+            <Route path="/data" element={<WorkInProgress component="Data"/>} />
+            <Route path="/provider" element={<WorkInProgress component="Provider"/>} />
+            <Route path="/help" element={<WorkInProgress component="Help"/>} />
+            <Route path="/register" element={<WorkInProgress component="Register"/>} />
+            <Route path="/signin" element={<WorkInProgress component="Sign in"/>} />
+            
+          </Routes>
+        </div>
+        </BrowserRouter>
     </div>
   );
 }
