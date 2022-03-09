9d17d52e9a45efe6a6312c6eec8b3a63a78b83cf
j_esteve <javier.esteve@ultratendency.com>
2022-03-09 11:50:26 +0100

    add home screen

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,13 @@ import { withTranslation } from "react-i18next";
 const Home = ({t}) => {
     return (
         <div className="home">
-            <div className="header">
+            <div className="home-header">
                 <h1>{t('home.welcome.title')}</h1>
                 <h2>{t('home.welcome.subtitle')}</h2>
             </div>
-            <div className="search"> TODO: Here goes Search component</div>
-            
-
+            <div className="home-search"> TODO: Here goes Search component</div>
+            <div className="home-article">TODO: put here what's new</div>
+            <div className="home-article">TODO: put here what is Gaia-X</div>
         </div>
     );
 }
