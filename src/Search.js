639057e125ed9c4149615a9e57f2ad40b0f82bd6
VladimirBelov <vladimir.belov@ultratendency.com>
2022-03-14 11:03:38 +0300

    search bar

---
+++ src/Search.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import { 
+    MDBCol
+} from "mdbreact";
+import './Search.css';
+
+class Home extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            searchText: '',
+        };
+    }
+
+    onChange(e) {
+        this.setState({
+            searchText: e.target.value
+        });
+    }
+
+    updateText(text) {
+        this.setState({
+            searchText: `${this.state.searchText} ${text}`
+        });
+    }
+
+    onKeyPressed(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code !== 13) return;
+        alert("Starting search");
+    }
+
+    searchProcessing() {
+        alert("Under construction");
+    }
+
+    render() {
+        return(
+            <div>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+                <center><h3>Search Service</h3></center>
+                <div className='example'>
+                    <MDBCol md="6">
+                        <input type="text" 
+                            id="searchElement"
+                            placeholder="Search..." 
+                            onKeyDown={(e) => this.onKeyPressed(e)}
+                            value={this.state.searchText}
+                            onChange={(e) => this.onChange(e)}
+                        />
+                        <button 
+                            type="submit" 
+                            onClick={() => this.searchProcessing()}>
+                                Advanced
+                        </button>
+                    </MDBCol>
+                </div>
+                        
+                <div className='example'>
+                    <MDBCol md="6">
+                        <button 
+                            type="submit" 
+                            onClick={() => this.updateText(" NOT=")}>
+                                NOT
+                        </button>
+                        <button 
+                            type="submit" 
+                            onClick={() => this.updateText(" Provider=")}>
+                                Provider
+                        </button>
+                        <button 
+                            type="submit" 
+                            onClick={() => this.updateText(" Storage=")}>
+                                Storage
+                        </button>
+                        <button 
+                            type="submit" 
+                            onClick={() => this.updateText(" Service=")}>
+                                Service
+                        </button>
+                        <button 
+                            type="submit" 
+                            onClick={() => this.updateText(" Compute=")}>
+                                Compute
+                        </button>
+                    </MDBCol>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Home;
