15ce970f0a40ccc28fa87d8b374fd685d72904e2
VladimirBelov <vladimir.belov@ultratendency.com>
2022-03-15 12:00:30 +0300

    styles for search bar

--- src/Search.js
+++ src/Search.js
@@ -37,42 +37,52 @@ class Home extends Component {
             <div>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
                 <div className='searchBarComponent'>
-                        <input type="text" 
+                    <div className='searchInputField'>     
+                        <input 
+                            type="text" 
                             id="searchElement"
                             placeholder="Search..." 
                             onKeyDown={(e) => this.onKeyPressed(e)}
                             value={this.state.searchText}
                             onChange={(e) => this.onChange(e)}
                         />
+                        <i class='fa fa-search'></i>
+                    </div>
                         <button 
+                            className='button-16'
                             type="submit" 
                             onClick={() => this.searchProcessing()}>
                                 Advanced
                         </button>
                 </div>
                         
-                <div className='searchBarComponent'>
+                <div className='additionalSearchOptions'>
                         <button 
+                            className='button-17'
                             type="submit" 
                             onClick={() => this.updateText(" NOT=")}>
                                 NOT
                         </button>
                         <button 
+                            className='button-17'
                             type="submit" 
                             onClick={() => this.updateText(" Provider=")}>
                                 Provider
                         </button>
                         <button 
+                            className='button-17'
                             type="submit" 
                             onClick={() => this.updateText(" Storage=")}>
                                 Storage
                         </button>
                         <button 
+                            className='button-17'
                             type="submit" 
                             onClick={() => this.updateText(" Service=")}>
                                 Service
                         </button>
                         <button 
+                            className='button-17'
                             type="submit" 
                             onClick={() => this.updateText(" Compute=")}>
                                 Compute
