a39b0ca40d2668752d170df6aeddecc6d0ad157c
jbendlin <johannes.bendlin@ultratendency.com>
2022-03-30 13:29:13 +0200

    Reduce duplicity

--- src/components/RegisterMailSent.js
+++ src/components/RegisterMailSent.js
@@ -9,22 +9,7 @@ const RegisterMailSent = (props) => {
     const location = useLocation();
     const formType = new URLSearchParams(location.search).get("formType");
 
-    const formOrganizationOne = () => {
-        return (
-        <div className="RegisterUser">
-            <div className="registerHelpText">
-                <h3>{props.t("form.formUserHeadline")}</h3>
-                <p>{props.t(`form.${formType}FormEmailSentHelp`)}</p>
-            </div>
-            <VerticalSteps current="3" numSteps="3"/>
-            <div className="registerInputs">
-                <p> {props.t("form.formEmailSentMessage")}</p>
-            </div>
-        </div>
-        )
-    };
-
-    const formUserThree = () => {
+    const formEmailSend = () => {
         return (
         <div className="RegisterUser">
             <div className="registerHelpText">
@@ -41,8 +26,7 @@ const RegisterMailSent = (props) => {
 
     return (
         <Modal>
-            { formType === "organization" && formOrganizationOne()}
-            { formType === "user" && formUserThree()}
+            {formEmailSend()}
         </Modal>
     
     );
