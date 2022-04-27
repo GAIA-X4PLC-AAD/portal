0dcb3c196dc25a43655b4211cabc3b76f7d905bb
Yury Beloglazov <yuri.beloglazov@ultratendency.com>
2022-04-27 14:22:10 +0300

    tmp commit for backup

--- src/components/RegisterUserViaDid.js
+++ src/components/RegisterUserViaDid.js
@@ -12,7 +12,7 @@ const RegisterUserViaDid = (props) => {
     const [img, setImg] = useState({});
 
     useEffect(  () => {
-        axios.get(configData.ONBOARDING_API_URI+`/register/user/did_register`)
+        axios.get(configData.EDGE_API_URI+`/register/user/did_register`)
         .then((body) => {
             let qrCodePath = body.data.qrCodePath;
             setImg(qrCodePath);
@@ -55,6 +55,7 @@ const RegisterUserViaDid = (props) => {
                             onAuthZFailed={onAuthZFailed}
                             onAuthZSuccess={onAuthZSuccess}
                             onAuthZWait={onAuthZWait}
+                            statusURL={configData.EDGE_API_URI + configData.uri_path.auth_status_path}
                         />
                         <img src={img} width="150px" height="150px" alt="Loading..."/>
                     </div>
