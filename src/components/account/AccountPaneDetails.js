e3837e3ba01902b45286a9f3d0706ff6c7c80d0b
jbendlin <johannes.bendlin@ultratendency.com>
2022-04-27 11:52:10 +0200

    Add User account details via edge-service

--- src/components/account/AccountPaneDetails.js
+++ src/components/account/AccountPaneDetails.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { withTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import "./Account.css";
+import configData from "../../config/config.json";
 
-const AccountPaneDetails = () => {
+const AccountPaneDetails = (props) => {
 
+    const [user, setUser] = useState(null);
 
-
-    const renderAccountPaneDetails = () => {
-        return (
-            <div className="account-pane-details">
-                details
-            </div>
-        )
-    };
+    useEffect (() => {
+        axios.get(configData.EDGE_API_URI + '/account/user').then((response) => {
+            console.log(response.data);
+            setUser(response.data);
+        }, (error) => {
+          alert('ko');
+        });
+    }, []);
 
     return (
-        renderAccountPaneDetails()
-    );
+            <div className="account-pane-details">
+                <div className="account-pane-details-image">
+                    <img src={user?.avatarImageLink} alt="User avatar image."/>
+                </div>
+                <div className="account-pane-details-body">
+                    <div className="account-pane-details-body-flex-col">
+                        <div className="account-pane-details-attribute">
+                            <div className="account-pane-details-attribute-label">{props.t("account.details.email")}</div>
+                            <div className="account-pane-details-attribute-box">{user?.email}</div>
+                        </div>
+                        <div className="account-pane-flex">
+                            <div className="account-pane-details-attribute">
+                                <div className="account-pane-details-attribute-label">{props.t("account.details.firstName")}</div>
+                                <div className="account-pane-details-attribute-box">{user?.name}</div>
+                            </div>
+                            <div className="account-pane-details-attribute">
+                                <div className="account-pane-details-attribute-label">{props.t("account.details.lastName")}</div>
+                                <div className="account-pane-details-attribute-box">{user?.surname}</div>
+                            </div>
+                        </div>
+                        <div className="account-pane-details-attribute">
+                            <div className="account-pane-details-attribute-label">{props.t("account.details.attributeOfVC")}</div>
+                            <div className="account-pane-details-attribute-box">{user?.attributeA}</div>
+                        </div>
+                        <div className="account-pane-details-attribute">
+                            <div className="account-pane-details-attribute-label">{props.t("account.details.attributeOfVC")}</div>
+                            <div className="account-pane-details-attribute-box">{user?.attributeB}</div>
+                        </div>
+                        <div className="account-pane-details-attribute">
+                            <div className="account-pane-details-attribute-label">{props.t("account.details.attributeOfVC")}</div>
+                            <div className="account-pane-details-attribute-box">{user?.attributeC}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
 }
 
-export default AccountPaneDetails;
+export default withTranslation()(AccountPaneDetails);
