b762aba0b1d3f96ef6640622f74e60a712116957
Yury Beloglazov <yuri.beloglazov@ultratendency.com>
2022-06-13 06:59:25 +0300

    preparation for ci/cd

--- src/components/registration/RegisterViaDID.js
+++ src/components/registration/RegisterViaDID.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { withTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import Modal from "../../Modal";
@@ -18,56 +18,56 @@ const RegisterViaDID = (props) => {
 
     const [showLoginFail, setShowLoginFail] = useState(false);
 
-    
-    useEffect(  () => {
-        axios.get(configData.EDGE_API_URI+`/register/user/did_register`) // RFCT: portal uri is at gaia-x.portal.local:8085 but frontend differs
-        .then((body) => {
-            let qrCodePath = body.data.qrCodePath;
-            setImg(qrCodePath);
-        })
-    },[img]);
+
+    useEffect(() => {
+        axios.get(configData.EDGE_API_URI + `/onboarding/register/user/did_register`)
+            .then((body) => {
+                let qrCodePath = body.data.qrCodePath;
+                setImg(qrCodePath);
+            })
+    }, [img]);
 
 
     const onAuthZSuccess = () => {
         // alert('onAuthZSuccess');
         navigate("/register/displayVC");
     }
-         
+
     const onAuthZFailed = () => {
         // alert('onAuthZFailed');
         // navigate("/loginfail");
         setShowLoginFail(true);
     }
-      
+
     const onAuthZWait = () => {
         console.log('onAuthZWait');
     }
-    
+
     const registerViaDID = () => {
         return (
-        <div className="RegisterUser">
-            <div className="registerHelpText">
-                <h3>{props.t("form.formUserHeadline")}</h3>
-                <p>{props.t(`form.formOrganizationProofOfOnboarding`)}</p>
-            </div>
-            <VerticalSteps current="2" numSteps="4"/>
-            <div className="registerInputs">
-                <p> {props.t("form.formOrganizationPleaseVerifyYourself")}</p>
-                <div className="registerViaQrCode">
+            <div className="RegisterUser">
+                <div className="registerHelpText">
+                    <h3>{props.t("form.formUserHeadline")}</h3>
+                    <p>{props.t(`form.formOrganizationProofOfOnboarding`)}</p>
+                </div>
+                <VerticalSteps current="2" numSteps="4" />
+                <div className="registerInputs">
+                    <p> {props.t("form.formOrganizationPleaseVerifyYourself")}</p>
+                    <div className="registerViaQrCode">
                         <AuthPolling
                             onAuthZFailed={onAuthZFailed}
                             onAuthZSuccess={onAuthZSuccess}
                             onAuthZWait={onAuthZWait}
                             statusURL={configData.EDGE_API_URI + configData.uri_path.auth_status_path}
                         />
-                        <LoginFail showAlertMessage={showLoginFail} message={props.t("form.formOrganizationNoProcuraError")}/>
-                        <img src={img} width="150px" height="150px" alt="Loading..."/>
+                        <LoginFail showAlertMessage={showLoginFail} message={props.t("form.formOrganizationNoProcuraError")} />
+                        <img src={img} width="150px" height="150px" alt="Loading..." />
                     </div>
-                <div className="formButtons">
-                    <button onClick={() => navigate("/register/IdP?mock=organization")}>{props.t("form.regViaDIDNoDID")}</button>
-                 </div>
+                    <div className="formButtons">
+                        <button onClick={() => navigate("/register/IdP?mock=organization")}>{props.t("form.regViaDIDNoDID")}</button>
+                    </div>
+                </div>
             </div>
-        </div>
         )
     };
 
@@ -75,7 +75,7 @@ const RegisterViaDID = (props) => {
         <Modal>
             {registerViaDID()}
         </Modal>
-    
+
     );
 
 }
