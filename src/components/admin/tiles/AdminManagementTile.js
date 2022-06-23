5bd0cebf19e549f25f75ca45799a687ac1f37b8d
j_esteve <javier.esteve@ultratendency.com>
2022-06-23 16:08:45 +0200

    fixing some style issues

--- src/components/admin/tiles/AdminManagementTile.js
+++ src/components/admin/tiles/AdminManagementTile.js
@@ -3,20 +3,22 @@ import ExpandableView from "../../expandable/ExpandableView";
 import PropTypes from 'prop-types';
 import ManagementHeader from "../tabs/ManagementHeader";
 import KeyValueDetails from "../tabs/KeyValueDetails";
+import { useResource } from "@axios-use/react";
 
 
-const AdminManagementTile = ({ input }) => {
+const AdminManagementTile = ({ input , searchRefresh}) => {
 
     const URL = process.env.REACT_APP_EDGE_API_URI + '/admin/pr/registrations';
     
     return (
-        <ExpandableView initiallyExpanded={false} view={<KeyValueDetails id={input.id} url_prefix={URL}/>} title={<ManagementHeader participant={input}/>} border={true} />
+        <ExpandableView initiallyExpanded={false} view={<KeyValueDetails id={input.id} url_prefix={URL} searchRefresh={searchRefresh}/>} title={<ManagementHeader participant={input}/>} border={true} />
     );
 
 }
 
 AdminManagementTile.propTypes = {
-    input: PropTypes.object
+    input: PropTypes.object,
+    searchRefresh: PropTypes.func
 }
 
 export default AdminManagementTile;
