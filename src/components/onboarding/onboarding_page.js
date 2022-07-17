a46d40d827da98ee69ee2698ca8ab3cc6e506fab
j_esteve <javier.esteve@ultratendency.com>
2022-07-17 22:40:52 +0200

    Adding translations to steps pane. And make it generic for type of registration.

--- src/components/onboarding/onboarding_page.js
+++ src/components/onboarding/onboarding_page.js
@@ -11,6 +11,7 @@ import styled from "styled-components";
 import Modal, { ModalProvider, BaseModalBackground } from "styled-react-modal";
 
 import OrganizationDetailsView from "./onboarding_provider";
+import StepsPane from "./steps_pane";
 
 
 const OnboardingPage = () => {
@@ -70,37 +71,10 @@ const OnboardingPage = () => {
         )
     }
 
-    const buildStepCardView = ({ stage, title, subtitle, isActive }) => {
-        return (
-            <Padding vertical='12px' paddingTop={stage == 1 ? '0px' : ''}>
-                <Card background='#fff' borderColor='#0' boxShadow={`0px ${isActive ? '3' : '2'}px 4px 0px rgb(29 36 48 / ${isActive ? '25' : '12'}%)`}>
-                    <Padding vertical='12px' horizontal='10px'>
-                        <Row alignItems='center'>
-                            <Circle radius='44px' background={isActive ? '#000094' : '#E9E9E9'} borderColor='#0'>
-                                <BodySmallBoldText color={isActive ? '#fff' : '#2A2A2A'}>{stage}</BodySmallBoldText>
-                            </Circle>
-                            <Padding horizontal='20px'>
-                                <Column>
-                                    <BodyBoldText>{title}</BodyBoldText>
-                                    <BodySmallText>{subtitle}</BodySmallText>
-                                </Column>
-                            </Padding>
-                            <Style flexGrow='1'></Style>
-                            <Circle radius='8px' background={isActive ? '#6BB324' : '#E9E9E9'} borderColor='#0' />
-                        </Row>
-                    </Padding>
-                </Card>
-            </Padding>
-        )
-    }
-
     const stepsPane = ({ activeStage = 1, isNextDisabled = false }) => {
         return (
             <>
-                {buildStepCardView({ stage: '1', title: 'Customer or provider', subtitle: 'Step 1', isActive: activeStage == 1 })}
-                {buildStepCardView({ stage: '2', title: 'Organization details', subtitle: 'Step 2', isActive: activeStage == 2 })}
-                {buildStepCardView({ stage: '3', title: 'Confirmation email', subtitle: 'Step 3', isActive: activeStage == 3 })}
-                {buildStepCardView({ stage: '4', title: 'Email notification', subtitle: 'Step 4', isActive: activeStage == 4 })}
+               <StepsPane type={customerOrOrganization} currentStage={activeStage} />
                 <Row>
                     <Padding vertical='32px'><MasterButton disabled={activeStage === 1} onClick={() => previousStage()}>Previous</MasterButton></Padding>
                     <Padding vertical='32px'><MasterButton disabled={isNextDisabled} onClick={() => nextStage()}>Next</MasterButton></Padding>
