b573289ef6f9e1d5fb9eac5d1e1fdd2389a2c791
j_esteve <javier.esteve@ultratendency.com>
2022-07-18 13:47:25 +0200

    Adding some translation

--- src/components/onboarding/onboarding_provider.js
+++ src/components/onboarding/onboarding_provider.js
@@ -1,14 +1,16 @@
 import axios from "axios";
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import PropTypes from 'prop-types';
 
 import Checkbox from "../../common/checkbox";
-import {  Column, Row, Style, Card, BodyText, ButtonText, H4LightText, HorizontalLine, OutlineButton, TextInput, Image, StyledModal } from "../../common/styles";
+import {  Column, Row, Style, Card, BodyText, ButtonText, H4LightText, HorizontalLine, OutlineButton, TextInput, Image } from "../../common/styles";
 import { Padding } from "../discovery/tabs/style";
-import { ModalProvider } from "styled-react-modal";
 import DidOnboardingView from "./onboarding_did";
+import { useTranslation } from "react-i18next";
 
 const OrganizationDetailsView = ({onSuccess}) => {
+
+    const {t} = useTranslation();
 
     const URL = process.env.REACT_APP_EDGE_API_URI + '/onboarding/register/organization/';
 
@@ -28,8 +30,6 @@ const OrganizationDetailsView = ({onSuccess}) => {
  }
 
   const onFormChanged = (e) => {
-    console.log('e.target.name: ' , e.target.name);
-    console.log('e.target.value: ' , e.target.value);
       const key = e.target.name;
       const value = getValue(e.target);
       setInput(values => ({ ...values, [key]: value }))
@@ -37,23 +37,29 @@ const OrganizationDetailsView = ({onSuccess}) => {
 
 
     const onFormSubmit = () => {
+        if (!input.document) {
+            setEMessage('Please upload a document');
+            return;
+        }
         if (formRef.current.reportValidity())
         {
-            console.log("Submit button was clicked so we'll submit the form");
-            axios.post(URL, input, {
+            let formData = new FormData();
+            formData.append('name', input.name);
+            formData.append('email', input.email);
+            formData.append('aisbl', input.aisbl||false);
+            formData.append('document', input.document);
+             axios.post(URL, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                     }
                 }).then (
                     response => {
-                        //move to next step
                         onSuccess();
                     }, 
                     error => {
                         console.log(error);
                         setEMessage(error.response.data);
-                        // remove onSuccess call
-                        onSuccess ();
+
                     });
         }
     }
@@ -65,19 +71,18 @@ const OrganizationDetailsView = ({onSuccess}) => {
             <Padding horizontal='20px'>
                 <Card background='#fff' borderColor='#0' boxShadow={`0px 2px 4px 0px rgb(29 36 48 / 12%)`}>
                     <Padding horizontal='24px'>
-                        <H4LightText>Do you want to register as a customer or provider?</H4LightText>
-                        <ButtonText color='#00A2E4'>Learn more</ButtonText>
+                        <H4LightText>{t('onboarding.organization_headline')}</H4LightText>
                         <HorizontalLine />
                         <Padding vertical='24px'>
                              <form  ref={formRef} noValidate>
                             <Column>
-                                <BodyText>Please upload your organization details or select express registration via DID.</BodyText>
-                                <input name='file' type="file" ref={fileRef} onChange={e=> onFormChanged(e)} hidden/>
+                                <BodyText>{t('onboarding.organization_body_text')}</BodyText>
+                                <input name='document' type="file" ref={fileRef} onChange={e=> onFormChanged(e)} hidden/>
                                 <Padding vertical='16px' alignSelf='start' onClick={e=>fileRef.current.click()}><OutlineButton>Upload</OutlineButton></Padding>
                                 <Padding vertical='16px' />
-                                <TextInput name='name' type="text" value={input.name||''}  placeholder="Organization Name"  onChange={(e)=>onFormChanged(e)} required/>
+                                <TextInput name='name' type="text" value={input.name||''}  placeholder={t('onboarding.organization_name_placeholder')}  onChange={(e)=>onFormChanged(e)} required/>
                                 <Padding vertical='4px' />
-                                <TextInput name='email' type="email" value={input.email||''} placeholder="Email"  onChange={(e)=>onFormChanged(e)} required/>
+                                <TextInput name='email' type="email" value={input.email||''} placeholder={t('onboarding.email_placeholder')}  onChange={(e)=>onFormChanged(e)} required/>
                                 <Padding vertical='8px' />
                                 {/* Checkbox */}
                                 <Row alignItems='center'>
@@ -88,16 +93,16 @@ const OrganizationDetailsView = ({onSuccess}) => {
                                         />
                                     </label>
                                     <Padding horizontal='4px' />
-                                    <BodyText>Apply for AISBL Membership</BodyText>
+                                    <BodyText>{t('onboarding.organization_aisbl')}</BodyText>
                                     <Padding horizontal='7px' />
                                     <Image objectFit='contain' src='/images/question-mark.svg' />
                                 </Row>
 
                                 <Padding vertical='28px'>
                                     <Row>
-                                        <OutlineButton onClick={()=> setDid(true)}>Registration via DID</OutlineButton>
+                                        <OutlineButton onClick={()=> setDid(true)}>{t('onboarding.register_did_button')}</OutlineButton>
                                         <Padding horizontal='10px' />
-                                        <OutlineButton onClick={e=>onFormSubmit()}>Send</OutlineButton>
+                                        <OutlineButton onClick={e=>onFormSubmit()}>{t('onboarding.send_button')}</OutlineButton>
                                     </Row>
                                 </Padding>
                             </Column>
