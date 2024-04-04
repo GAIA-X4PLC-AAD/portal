9b70b01cd238dc9f77ff55708536311108621bf1
Novak Zsolt <zsolt.gyorgy-novak@msg.group>
2024-04-04 12:17:02 +0300

    solved Review remarks
    Signed-off-by: Novak Zsolt <zsolt.gyorgy-novak@msg.group>

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ const App = (props) => {
             <Routes>
               <Route path="/" element={
                 <Column>
-                  {/* <Home /> */}
                   <Home />
                 </Column>
               }
