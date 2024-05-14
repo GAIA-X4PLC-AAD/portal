5365f2cf25cf9c8e0658ae983d45453b718b5348
Huber-David <david.huber@msg.group>
2024-05-14 18:40:04 +0200

    list ontologies. some bases and contributors are still missing
    Signed-off-by: Huber-David <david.huber@msg.group>

--- src/components/shapesAndOntologies/ShapesAndOntologies.tsx
+++ src/components/shapesAndOntologies/ShapesAndOntologies.tsx
@@ -1,14 +1,43 @@
 import styles from "./ShapesAndOntologies.module.css";
 import Title from "../Title/Title";
 import { useTranslation } from "react-i18next";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContextProvider";
 import car from "../../assets/car.gif";
+import { ApiService } from "../../services/ApiService";
+import { mapOntologies, mapShapesAndOntologies, Ontology } from "../../utils/dataMapper";
+import SelfDescriptionCard from "../cards/SelfDescriptionCard";
+import { AuthContextValues } from "../../context/AuthContextValues";
 
 const ShapesAndOntologies = () => {
     const { t } = useTranslation();
     const authContext = useContext(AuthContext);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [ontologies, setOntologies] = useState<Ontology[]>([]);
+
+    const fetchOntologies = async (authContext: AuthContextValues) => {
+        const response = await ApiService.getAllSchemas(authContext);
+        const ontologiesStringArray = mapShapesAndOntologies(response);
+        const promises = ontologiesStringArray.map((item) => ApiService.getSchemaWithId(authContext, item));
+        return await Promise.all(promises);
+    };
+
+    useEffect(() => {
+        const loadOntologies = async () => {
+            setIsLoading(true);
+            try {
+                const ontologyPromises = await fetchOntologies(authContext);
+                const ontologiesDetailed = mapOntologies(ontologyPromises);
+                setOntologies(ontologiesDetailed);
+            } catch (error) {
+                console.error("Error fetching self descriptions:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        loadOntologies();
+    }, []);
 
     return (
         <div>
@@ -17,13 +46,22 @@ const ShapesAndOntologies = () => {
                     <Title>{t("left-menu.shapesAndOntologies")}</Title>
                 </div>
             </header>
-            <div className={styles["resource-content-container"]}>
+            <div className={styles["shapesAndOntologies-content-container"]}>
                 {authContext.isAuthenticated && (
                     <div className={styles.content}>
                         <div>
-                            {isLoading && (
+                            {!isLoading && ontologies.length > 0 ? (
+                                ontologies.map((ontology, index) => (
+                                    <SelfDescriptionCard
+                                        key={index}
+                                        label={t("ontologies.title")}
+                                        name={ontology.base}
+                                        description={<> {"Label: " + ontology.label} <br /> {"Version: " + ontology.version} <br /> {"Contributor: " + ontology.contributors.join(", ")} </>}                                        selfDescription={ontology}
+                                    />
+                                ))
+                            ) : (
                                 <div className="newCarLoader">
-                                    <img src={car} alt="loading..." className="car" />
+                                    <img src={car} alt="loading..." className="car"/>
                                 </div>
                             )}
                         </div>
--- src/utils/dataMapper.ts
+++ src/utils/dataMapper.ts
@@ -72,3 +72,100 @@ export function mapResources(selfDescriptions: ResourceInput): Resource[] {
     claimsGraphUri : p.claimsGraphUri
   }));
 }
+
+// Interfaces and Mappers for Shapes and Ontologies
+export interface Class {
+  name: string;
+  label: string;
+  subClasses: any[];
+}
+
+export interface Ontology {
+  base: string;
+  contributors: string[];
+  label: string;
+  version: string;
+  classes: Class[];
+  claimsGraphUri: string;
+}
+
+export interface ShapesAndOntologiesInput {
+  ontologies: string[];
+  shapes: string[];
+  vocabularies: string[];
+}
+
+export const  mapShapesAndOntologies = (response: ShapesAndOntologiesInput): string[] => {
+  return response.ontologies.map((ontology) => ontology);
+}
+
+export const mapOntologies = (response: []): Ontology[] => {
+  return response.map((item) => {
+    return parseOntology(item);
+  });
+}
+
+const parseOntology = (data: string): Ontology => {
+  const ontologyToReturn: Ontology = {
+    base: "",
+    contributors: [],
+    label: "",
+    version: "",
+    classes: []
+  };
+
+  const baseMatch = data.match(/@base\s+<([^>]+)>/);
+  ontologyToReturn.base = baseMatch ? baseMatch[1] : "No base found";
+
+  const contributorsMatch = data.match(/contributor\s+"([^"]+)"(\s*,\s*"([^"]+)")*(\s*;\s*)?/);
+  if (contributorsMatch) {
+    let contributors = [contributorsMatch[1]];
+
+    const additionalMatches = data.match(/,\s*"([^"]+)"/g);
+
+    if (additionalMatches) {
+      additionalMatches.forEach(match => {
+        const nameMatch = match.match(/"([^"]+)"/);
+        if (nameMatch) {
+          contributors.push(nameMatch[1]);
+        }
+      });
+    }
+    ontologyToReturn.contributors = contributors
+  }
+
+  const labelMatch = data.match(/rdfs:label\s+"([^"]+)"/);
+  ontologyToReturn.label = labelMatch ? labelMatch[1] : "No label found";
+
+  const versionMatch = data.match(/owl:versionInfo\s+([\d.]+)/);
+  ontologyToReturn.version = versionMatch ? versionMatch[1] : "No version found";
+
+  const classes: Class[] = [];
+  const classMatches = data.match(/<([^>]+)>\s+a\s+rdfs:Class\s*;/g);
+  if (classMatches) {
+    classMatches.forEach(classMatch => {
+      const classObj: Class = {
+        name: '',
+        label: "",
+        subClasses: []
+      };
+      const className = classMatch.match(/<([^>]+)>/)[1];
+      classObj.name = className;
+
+      const labelMatch = data.match(new RegExp(`<${className}>\\s+a\\s+rdfs:Class\\s*;\\s+rdfs:label\\s+"([^"]+)"`));
+      classObj.label = labelMatch ? labelMatch[1] : "No label found";
+
+      const subClassOfMatches = data.match(new RegExp(`<${className}>\\s+a\\s+rdfs:Class\\s*;[^]+?rdfs:subClassOf\\s+([^;]+)`, 'g'));
+      if (subClassOfMatches) {
+        classObj.subClasses = subClassOfMatches.map(subClassMatch => subClassMatch.match(/rdfs:subClassOf\s+([^;]+)/)[1]);
+      }
+
+      classes.push(classObj);
+    });
+  }
+  ontologyToReturn.classes = classes;
+
+  console.log('Parsed ontology: ', ontologyToReturn);
+
+  return ontologyToReturn;
+}
