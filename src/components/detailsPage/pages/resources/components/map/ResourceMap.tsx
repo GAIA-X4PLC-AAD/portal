05f65be89667e7f28eedea966113a88c48be91df
Huber-David <david.huber@msg.group>
2024-10-01 11:45:22 +0200

    add vcmap to resource details
    Signed-off-by: Huber-David <david.huber@msg.group>

--- src/components/detailsPage/pages/resources/components/map/ResourceMap.tsx
+++ src/components/detailsPage/pages/resources/components/map/ResourceMap.tsx
@@ -1,18 +1,16 @@
-import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import Title from 'components/Title/Title';
+
+import VcMapComponent from '../../../../../VcMapComponent/VcMapComponent';
 
 import styles from './ResourceMap.module.css';
 
 const ResourceMap = () => {
-  const handleEnlarge = () => {}
-
   return (
     <div className={styles['map-card-container']}>
       <div className={styles['title']}>
         <Title>Map</Title>
-        {/*<OpenInFullIcon onclick={handleEnlarge}/>*/}
       </div>
-      {/*<VcMapComponent/>*/}
+      <VcMapComponent/>
     </div>
   );
 }
