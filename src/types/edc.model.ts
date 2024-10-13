c774acdf0aa5057846613a720f19ba1dbad8569c
Zoltan Magyari <codesage.control175@passmail.net>
2024-10-13 22:23:00 +0300

    Data Transfer
    Signed-off-by: Zoltan Magyari <codesage.control175@passmail.net>

--- src/types/edc.model.ts
+++ src/types/edc.model.ts
@@ -9,3 +9,34 @@ export type DataTransferInputProps = {
     account: string;
   }
 }
+
+export interface ContractInformation {
+  contractDefinitionId: string | null;
+  assetNameFull: string | null;
+}
+
+export interface AgreementInformation {
+  contractNegotiationUID: string;
+}
+
+export interface ContractAgreementInformation {
+  contractAgreementUID: string | null;
+  state: string | null;
+}
+
+export interface TransferProcessInformation {
+  transferProcessId: string | null;
+}
+
+export type TransferStates =
+  'STARTED'
+  | 'COMPLETED'
+  | 'TERMINATED'
+  | 'DEPROVISIONED'
+  | 'INITIATED'
+  | 'PROVISIONED'
+  | 'REQUESTED'
+
+export interface TransferStateInformation {
+  state: TransferStates;
+}
