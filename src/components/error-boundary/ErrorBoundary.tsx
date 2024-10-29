52dea0f8febdff78b1d2400dcd509db2f6f32c13
Keresztes Zsolt <178369172+zsoltker@users.noreply.github.com>
2024-10-29 14:19:57 +0200

    error notification working version 1 -- small refinements needed
    Signed-off-by: Keresztes Zsolt <178369172+zsoltker@users.noreply.github.com>

--- src/components/error-boundary/ErrorBoundary.tsx
+++ src/components/error-boundary/ErrorBoundary.tsx
@@ -11,30 +11,30 @@ const ErrorBoundary: React.FC<ErrorBoundaryProps> = ({ children }) => {
 
   const handleGlobalError = (event: ErrorEvent) => {
     event.preventDefault();
+    event.error.captured = true;
 
-    // Only notify if the error message is unique
     const errorMessage = event.error.message || 'An unknown error occurred';
-    if (errorMessage !== lastErrorRef.current) {
-      lastErrorRef.current = errorMessage;
-      notify({
-        messageType: 'ERROR',
-        message: errorMessage,
-      });
-    }
+
+    showNotification(errorMessage)
   };
 
   const handleUnhandledRejection = (event: PromiseRejectionEvent) => {
     event.preventDefault();
 
     const errorMessage = event.reason instanceof Error ? event.reason.message : String(event.reason);
-    if (errorMessage !== lastErrorRef.current) {
-      lastErrorRef.current = errorMessage;
-      notify({
-        messageType: 'ERROR',
-        message: errorMessage,
-      });
-    }
+    showNotification(errorMessage)
   };
+
+  const showNotification = (errorMessage: string) => {
+    // Only notify if the error message is unique
+    //   if (errorMessage !== lastErrorRef.current) {
+    lastErrorRef.current = errorMessage;
+    notify({
+      messageType: 'ERROR',
+      message: errorMessage,
+    });
+    //  }
+  }
 
   useEffect(() => {
     window.addEventListener('error', handleGlobalError);
