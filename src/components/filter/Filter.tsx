80d4054a690454edf3c87ab6d2dbbc7f18f58d0d
Zsolt-Novak <zsolt.gyorgy-novak@msg.group>
2025-01-15 16:56:08 +0200

    added filter colapse feature and selected filters counter

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,25 +1,46 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
-import Title from '../../common/components/fields/title/Title';
-import { Asset } from '../resources/helpers/resourceFilterHelper';
+import Title from "../../common/components/fields/title/Title";
+import { Asset } from "../resources/helpers/resourceFilterHelper";
 
-import styles from './Filter.module.css';
-import { FilterSection } from './FilterSection';
+import styles from "./Filter.module.css";
+import { FilterSection } from "./FilterSection";
+import { SpecificFilterSection } from "./SpecificFilterSection";
 
 interface IFilter {
-    typeAssets: Asset[];
-    formatAssets: Asset[];
-    vendorAssets: Asset[];
-    updateAssetFilter: (asset: Asset) => void;
-    visible?: boolean;
+  typeAssets: Asset[];
+  formatAssets: Asset[];
+  vendorAssets: Asset[];
+  updateAssetFilter: (asset: Asset) => void;
+  visible?: boolean;
 }
 
-const Filter: FC<IFilter> = ({ typeAssets, formatAssets, vendorAssets, updateAssetFilter, visible }) => (
+const Filter: FC<IFilter> = ({
+  typeAssets,
+  formatAssets,
+  vendorAssets,
+  updateAssetFilter,
+  visible,
+}) => (
   <aside className={visible ? styles.visible : styles.hidden}>
-    <Title className={styles.filterTitle}>Filter</Title>
-    <FilterSection subtitle="Type" assets={typeAssets} updateAssetFilter={updateAssetFilter}/>
-    <FilterSection subtitle="Format" assets={formatAssets} updateAssetFilter={updateAssetFilter}/>
-    <FilterSection subtitle="Vendor" assets={vendorAssets} updateAssetFilter={updateAssetFilter}/>
+    <Title className={styles.filterTitle}>Filters</Title>
+    <FilterSection
+      subtitle="Type"
+      assets={typeAssets}
+      updateAssetFilter={updateAssetFilter}
+    />
+    <FilterSection
+      subtitle="Format"
+      assets={formatAssets}
+      updateAssetFilter={updateAssetFilter}
+    />
+    <FilterSection
+      subtitle="Vendor"
+      assets={vendorAssets}
+      updateAssetFilter={updateAssetFilter}
+    />
+    <Title className={styles.filterTitle}>Specific Filters</Title>
+    <SpecificFilterSection subtitle="Specific Filter Subtitle" />
   </aside>
 );
 
