6ba579f38f9338197d173d273415969088e916fa
Huber-David <david.huber@msg.group>
2025-02-28 13:54:48 +0100

    update service tests regarding the download button
    Signed-off-by: Huber-David <david.huber@msg.group>

--- tests/components/serviceOfferings/components/ServiceOfferingActions.test.tsx
+++ tests/components/serviceOfferings/components/ServiceOfferingActions.test.tsx
@@ -1,25 +1,80 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitFor, screen } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 
+import { ServiceOfferingDetailsContext } from '../../../../src/components/context/ServiceOfferingDetailsContext';
 import ServiceOfferingActions from '../../../../src/components/serviceOfferings/components/ServiceOfferingActions';
+import { getAllSelfDescriptionDetails } from '../../../../src/helpers/selfDescriptionDataFlow';
+import { downloadFile } from '../../../../src/utils/fileUtils';
+
+jest.mock('../../../../src/helpers/selfDescriptionDataFlow');
+jest.mock('../../../../src/utils/fileUtils', () => ({
+  downloadFile: jest.fn(),
+}));
+
+const ComponentUnderTest = (mockDetails) => {
+  return render(
+    <ServiceOfferingDetailsContext.Provider value={mockDetails}>
+      <ServiceOfferingActions />
+    </ServiceOfferingDetailsContext.Provider>
+  );
+}
 
 describe('ServiceOfferingActions', () => {
-  it('renders correctly with all buttons', () => {
-    const { getByText } = render(<ServiceOfferingActions/>);
+  it('shows download button when serviceOfferingDetails exist and response length is greater than 0', async () => {
+    const mockDetails = { serviceOfferingDetails: { claimsGraphUri: 'test-uri' } };
+    const mockResponse = [{ holder: 'holder1' }];
+    (getAllSelfDescriptionDetails as jest.Mock).mockResolvedValue(mockResponse);
 
-    expect(getByText('Actions')).toBeInTheDocument();
-    expect(getByText('resources.download-description')).toBeInTheDocument();
-    expect(getByText('details.view-graph')).toBeInTheDocument();
-    expect(getByText('details.sidebar-buy-button')).toBeInTheDocument();
+    const { getByText } = ComponentUnderTest(mockDetails);
+
+    await waitFor(() => {
+      expect(getByText('resources.download-description')).toBeInTheDocument();
+      expect(getByText('Actions')).toBeInTheDocument();
+      expect(getByText('details.view-graph')).toBeInTheDocument();
+      expect(getByText('details.sidebar-buy-button')).toBeInTheDocument();
+    });
   });
 
-  it('calls appropriate function when download description button is clicked', () => {
-    const { getByText } = render(<ServiceOfferingActions/>);
-    const button = getByText('resources.download-description');
+  it('does not show download button when serviceOfferingDetails do not exist', () => {
+    const mockDetails = { serviceOfferingDetails: null };
 
-    fireEvent.click(button);
-    // Add any expected behavior verification for the click here
+    const { queryByText } = ComponentUnderTest(mockDetails);
+
+    expect(queryByText('resources.download-description')).not.toBeInTheDocument();
+    expect(queryByText('Actions')).toBeInTheDocument();
+    expect(queryByText('details.view-graph')).toBeInTheDocument();
+    expect(queryByText('details.sidebar-buy-button')).toBeInTheDocument();
+  });
+
+  it('does not show download button when response length is 0', async () => {
+    const mockDetails = { serviceOfferingDetails: { claimsGraphUri: 'test-uri' } };
+    const mockResponse = [];
+    (getAllSelfDescriptionDetails as jest.Mock).mockResolvedValue(mockResponse);
+
+    const { queryByText } = ComponentUnderTest(mockDetails);
+
+    await waitFor(() => {
+      expect(queryByText('resources.download-description')).not.toBeInTheDocument();
+      expect(queryByText('Actions')).toBeInTheDocument();
+      expect(queryByText('details.view-graph')).toBeInTheDocument();
+      expect(queryByText('details.sidebar-buy-button')).toBeInTheDocument();
+    });
+  });
+
+  it('calls appropriate function when download description button is clicked', async () => {
+    const mockDetails = { serviceOfferingDetails: { claimsGraphUri: 'test-uri' } };
+    const mockResponse = [{ holder: 'holder1' }];
+
+    (getAllSelfDescriptionDetails as jest.Mock).mockResolvedValue(mockResponse);
+
+    const { getByText } = ComponentUnderTest(mockDetails);
+
+    await waitFor(() => {
+      const button = getByText('resources.download-description');
+      fireEvent.click(button);
+    });
+    expect(downloadFile).toHaveBeenCalledTimes(1)
   });
 
   it('calls appropriate function when view graph button is clicked', () => {
