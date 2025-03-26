0607a220ded90f7081d8080bf442b775dd82b9ed
Huber-David <david.huber@msg.group>
2025-03-26 21:44:50 +0100

    update support text
    Signed-off-by: Huber-David <david.huber@msg.group>

--- src/components/support/SupportPage.tsx
+++ src/components/support/SupportPage.tsx
@@ -1,7 +1,9 @@
+/* test coverage not required */
 import Main from 'common/components/layouts/Main';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import TextEntry from '../../common/components/fields/entry/TextEntry';
 import Header from '../../common/components/header/Header';
 
 const SupportPage: FC = () => {
@@ -11,6 +13,9 @@ const SupportPage: FC = () => {
     <>
       <Header title={t('left-menu.support')}/>
       <Main>
+        <TextEntry name={''} value={t('support.text-start')} />
+        <TextEntry name={''} value={t('support.text-mid')} />
+        <TextEntry name={''} value={t('support.text-end')} />
       </Main>
     </>
   );
