b32cbcf7e6422e3fa008bdb4dfccce3bc5bfb1c6
Huber-David <david.huber@msg.group>
2025-03-26 21:32:47 +0100

    update about text
    Signed-off-by: Huber-David <david.huber@msg.group>

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -1,3 +1,4 @@
+/* test coverage not required */
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -12,7 +13,9 @@ const AboutPage: FC = () => {
     <>
       <Header title={t('left-menu.about')}/>
       <Main>
-        <TextEntry name={'info'} value={t('about.description')} />
+        <TextEntry name={''} value={t('about.text-start')} />
+        <TextEntry name={''} value={t('about.text-mid')} />
+        <TextEntry name={''} value={t('about.text-end')} />
       </Main>
     </>
   );
